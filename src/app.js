31b65f365287f0d8c607426227174410d5c6aebf
dipindas-a-h <dipindasofficial@gmail.com>
2024-04-19 16:38:07 +0530

    necxt

--- src/app.js
+++ src/app.js
@@ -65,9 +65,12 @@ app.use(
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // Exclude "Content-Type" and "Authorization" headers from the CORS preflight response
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+});
 app.use(cookieParser());
 app.use("/public", express.static("public"));
 app.use('/initial',configRouter)
